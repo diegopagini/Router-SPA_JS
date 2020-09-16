87f41bfcde9d61c2a8e2f3b1648e05013a4b6d66
Diego Pagini <diegopagini777@gmail.com>
2020-09-16 07:27:27 -0300

    terminando el curso

---
+++ js/Paths.js
@@ -0,0 +1,18 @@
+const PATHS = {
+  home: {
+      path: "/",
+      template: "<h1>Home 🏠</h1>",
+  },
+  about: {
+      path: "/about",
+      template: "<h1>Sobre mi 👨🏻‍💻</h1>",
+  },
+  contact: {
+      path: "/contact",
+      template: "<h1>Contacto 📲</h1>",
+  },
+  error: {
+      path: "/",
+      template: "<h1>Error ⛔️</h1>",
+  },
+}
